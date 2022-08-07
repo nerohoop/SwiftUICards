020f433dec16b6b9703a8723579028abb74a6728
Jiang Xiao <nerohoop@gmail.com>
2022-08-07 12:42:51 +0100

    Initial Commit

---
+++ CardUIComponents/CardUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardUIComponentsApp.swift
+//  CardUIComponents
+//
+//  Created by Xiao Jiang on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CardUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardUIComponents/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardUIComponents
+//
+//  Created by Xiao Jiang on 07/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
